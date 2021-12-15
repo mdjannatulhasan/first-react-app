ecf2d7721bd03914576919bde33a8f17a3f8b3d8
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2021-12-15 13:02:00 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning React
+          Learning React basic
         </a>
       </header>
     </div>
