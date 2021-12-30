b0fc5f59fdd83fd3d398e6fc942abc6490df28e2
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2021-12-30 20:21:19 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React basic
-        </a>
-      </header>
-    </div>
-  );
+  const products = [{name:'Photoshop', price:'100$'}]
+    return (
+        <div className="App">
+            <header className="App-header">
+            <Person></Person>
+            <Product name={products[0].name}></Product>
+            </header>
+            <Person></Person>
+            <Product name={products[0].name}></Product>
+        </div>
+    );
 }
-
+function Person(){
+  return(
+    <div>
+      <h1>Hello there</h1>
+    </div>
+  )
+}
+function Product(props){
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px', 
+    backgroundColor: 'lightgray',
+    height:'200px',
+    width:'200px',
+    float:'left'
+  }
+  return (
+    <div style={productStyle}>
+      <h2>{props.name}</h2>
+    </div>
+  )
+}
 export default App;
